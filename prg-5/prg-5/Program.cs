9d164d8434f15c67581753f6914cf03d0a74bfd1
Chethan P <132081469+chethanp1@users.noreply.github.com>
2023-12-29 09:46:29 +0530

    Add files via upload

---
+++ prg-5/prg-5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prg_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String:");
+            string orinalstr = Console.ReadLine();
+            string reversestr = string.Empty;
+            for (int i = orinalstr.Length - 1; i >= 0; i--)
+            {
+                reversestr += orinalstr[i];
+            }
+            Console.WriteLine("Result:" + reversestr);
+            Console.ReadLine();
+
+        }
+    }
+}
