e3ba976b55f4a7ab5b4bd5ab6c1302c0db712666
Chethan P <132081469+chethanp1@users.noreply.github.com>
2023-12-29 09:36:35 +0530

    Add files via upload

---
+++ prg-3/prg-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prg_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numerator");
+            int p = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the denominator");
+            int q = int.Parse(Console.ReadLine());
+            try
+            {
+                int r = p / q;
+                Console.WriteLine("The value of Quotient is {0}", r);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed");
+            }
+            Console.ReadLine();
+        }
+    }
+}
