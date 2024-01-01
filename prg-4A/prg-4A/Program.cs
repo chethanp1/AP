16b77338f5892fde7ac4196845a7f2d71f5d0c53
Chethan P <132081469+chethanp1@users.noreply.github.com>
2024-01-01 10:24:30 +0530

    Add files via upload

---
+++ prg-4A/prg-4A/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prg_4A
+{
+    class Baseclass
+    {
+        public virtual string city()
+        {
+            return "BMSCE";
+        }
+    }
+    class derivedclass : Baseclass
+    {
+        public override string city()
+        {
+            return "BMSIT";
+        }
+
+    }
+    class program6
+    {
+        static void Main(string[] args)
+        {
+            derivedclass o = new derivedclass();
+            string city = o.city();
+            Console.WriteLine("college name is {0}", city);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ prg-4B/prg-4B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prg_4B
+{
+    public delegate int deli(int n, int m);
+    public class pgm9
+    {
+        public int sum(int a, int b)
+            {
+                return a + b;
+            }
+         public int diff(int a, int b)
+            {
+                return a - b;
+            }
+    }
+    class program
+    {
+        static void Main(String[] argm)
+        {
+            pgm9 p1 = new pgm9();
+            pgm9 p2 = new pgm9();
+            deli d = p1.sum;
+            int i = d(10, 20);
+
+            deli f = p2.diff;
+            int j = f(40, 20);
+
+            Console.WriteLine("The sum is " + i + "\n difference is " + j);
+            Console.ReadLine();
+        }
+    }
+}
